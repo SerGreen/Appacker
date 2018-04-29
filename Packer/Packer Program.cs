0c502c2750ba9857d7e8ca74a047942ecc533968
SerGreen <sergreeny@gmail.com>
2018-04-29 03:59:24 +0300

    Packing and unpacking works

---
+++ Packer/Packer Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Packer
+{
+    class Program
+    {
+        private const string USAGE = "packer.exe <path_to_packed_app> <local_path_to_main_exe> <path_to_folder_for_packing>";
+
+        //string pathToPackedApp, string localPathToMainExe, string pathToFolderWithApp
+        static void Main(string[] args)
+        {
+            #region == Arguments check and assignment ==
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Arguments are missing. Usage:");
+                Console.WriteLine(USAGE);
+                return;
+            }
+
+            string pathToPackedApp = args[0];
+            string localPathToMainExe = args[1];
+            string pathToFolderWithApp = args[2];
+            
+            if (!Directory.Exists(pathToFolderWithApp))
+            {
+                Console.WriteLine("Specified directory with application does not exist.");
+                return;
+            }
+
+            if (!File.Exists(Path.Combine(pathToFolderWithApp, localPathToMainExe)))
+            {
+                Console.WriteLine("Main executable does not exist in app directory.");
+                return;
+            }
+
+            // Check if the provided path where we gonna save packed exe is valid
+            FileStream testFile = null;
+            try
+            {
+                testFile = File.Create(pathToPackedApp + ".temp", 1, FileOptions.DeleteOnClose);
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Invalid path to packed executable.");
+                return;
+            }
+            finally
+            {
+                testFile?.Close();
+            }
+            #endregion
+
+            // Get all files in the application folder (incl. sub-folders)
+            List<string> filesToPack = GetFilesRecursively(pathToFolderWithApp);
+            
+            // Transform absolute paths into relative ones
+            for (int i = 0; i < filesToPack.Count; i++)
+                filesToPack[i] = filesToPack[i].Replace(pathToFolderWithApp, string.Empty).TrimStart('\\');
+
+            PackApp(pathToPackedApp, pathToFolderWithApp, localPathToMainExe, filesToPack);
+        }
+
+        /// <summary>
+        /// Packs given files into a single executable file
+        /// </summary>
+        /// <param name="pathToPackedApp">Path where packed .exe will be created</param>
+        /// <param name="pathToFolderWithApp">Path to the directory that contains application files that will be packed</param>
+        /// <param name="localPathToMainExe">Relative path to the executable file that will be launched when the packed app is launched</param>
+        /// <param name="filesToPack">List of relative paths to all files of the app that is being packed</param>
+        private static void PackApp(string pathToPackedApp, string pathToFolderWithApp, string localPathToMainExe, List<string> filesToPack)
+        {
+            using (var packedExe = new BinaryWriter(File.Open(pathToPackedApp, FileMode.Create, FileAccess.Write)))
+            {
+                // Write wrapper app (unpacker.exe) 
+                // and the key-word mark that will help to separate wrapper app bytes from data bytes
+                packedExe.Write(Resource.unpacker);                     // byte[]
+                packedExe.Write(Encoding.UTF8.GetBytes("<SerGreen>"));    // byte[]
+
+                // Write relative path to the main executable of the packed app
+                packedExe.Write(localPathToMainExe);                    // string
+
+                // Write all packed files to the end of wrapper app .exe file
+                foreach (string filePath in filesToPack)
+                {
+                    byte[] data = File.ReadAllBytes(Path.Combine(pathToFolderWithApp, filePath));
+                    packedExe.Write(filePath);                          // string
+                    packedExe.Write(data.Length);                       // int
+                    packedExe.Write(data);                              // byte[]
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get all files paths from the directory and its subdirectories
+        /// </summary>
+        /// <param name="dirPath">Path to the directory</param>
+        /// <returns>List of absolute paths to all files within given directory and its subdirectories</returns>
+        private static List<string> GetFilesRecursively(string dirPath)
+        {
+            List<string> files = new List<string>();
+            files.AddRange(Directory.GetFiles(dirPath));
+
+            foreach (string subDir in Directory.GetDirectories(dirPath))
+                files.AddRange(GetFilesRecursively(subDir));
+
+            return files;
+        }
+    }
+}
---
+++ Unpacker/Unpacker Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unpacker
+{
+    class Program
+    {
+        private static string tempDir = null;
+
+        static void Main(string[] args)
+        {
+
+#if DEBUG
+            Console.WriteLine("Running in Debug");
+            Console.WriteLine("Attach to process now and press Enter...");
+            Console.ReadLine();
+#endif
+
+            // Subscribe for exit event to delete tempDir
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(Unpacker_ProcessExit);
+
+            // Create temp directory to store unpacked files
+            while (tempDir == null || Directory.Exists(tempDir))
+                tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(tempDir);
+
+            // Open self exe file as byte stream and unpack data that is appended to the end of exe
+            string selfExePath = System.Reflection.Assembly.GetEntryAssembly().Location;
+            string pathToMainExe = null;
+            using (var me = new BinaryReader(File.OpenRead(selfExePath)))
+            {
+                // Find the end of the actual .exe
+                byte[] pattern = Encoding.UTF8.GetBytes("<SerGreen>");
+                long pos = FindPatternPosition(me.BaseStream, pattern);
+                me.BaseStream.Seek(pos + pattern.Length, SeekOrigin.Begin);
+                
+                pathToMainExe = me.ReadString();
+
+                // Extract files until the end of stream is reached
+                while(me.BaseStream.Position < me.BaseStream.Length)
+                {
+                    string path = me.ReadString();
+                    int length = me.ReadInt32();
+                    byte[] data = me.ReadBytes(length);
+
+                    string dir = Path.GetDirectoryName(path);
+                    if (!string.IsNullOrWhiteSpace(dir) && !Directory.Exists(Path.Combine(tempDir, dir)))
+                        Directory.CreateDirectory(Path.Combine(tempDir, dir));
+
+                    File.WriteAllBytes(Path.Combine(tempDir, path), data);
+                }
+            }
+
+            // Launch unpacked app
+            Process proc = Process.Start(Path.Combine(tempDir, pathToMainExe));
+
+            // Wait until app exits to delete its files from temp directory
+            proc.WaitForExit();
+        }
+
+        // Exit event. Delete tempDir recursively
+        private static void Unpacker_ProcessExit(object sender, EventArgs e)
+        {
+            if (tempDir != null && Directory.Exists(tempDir))
+                Directory.Delete(tempDir, true);
+        }
+
+
+        #region Find pattern position in stream methods
+        // Solution from here https://stackoverflow.com/questions/1471975
+        /// <summary>
+        /// Search for first occurance of byte-sequence pattern in stream
+        /// </summary>
+        /// <param name="stream">Stream to search in</param>
+        /// <param name="byteSequence">Pattern to search for</param>
+        /// <returns>Index of sequence start</returns>
+        public static long FindPatternPosition(Stream stream, byte[] byteSequence)
+        {
+            if (byteSequence.Length > stream.Length)
+                return -1;
+
+            byte[] buffer = new byte[byteSequence.Length];
+
+            BufferedStream bufStream = new BufferedStream(stream, byteSequence.Length);
+            int i;
+            while ((i = bufStream.Read(buffer, 0, byteSequence.Length)) == byteSequence.Length)
+            {
+                if (byteSequence.SequenceEqual(buffer))
+                    return bufStream.Position - byteSequence.Length;
+                else
+                    bufStream.Position -= byteSequence.Length - PadLeftSequence(buffer, byteSequence);
+            }
+
+            return -1;
+        }
+
+        private static int PadLeftSequence(byte[] bytes, byte[] seqBytes)
+        {
+            int i = 1;
+            while (i < bytes.Length)
+            {
+                int n = bytes.Length - i;
+                byte[] aux1 = new byte[n];
+                byte[] aux2 = new byte[n];
+                Array.Copy(bytes, i, aux1, 0, n);
+                Array.Copy(seqBytes, aux2, n);
+                if (aux1.SequenceEqual(aux2))
+                    return i;
+                i++;
+            }
+            return i;
+        }
+        #endregion
+    }
+}
